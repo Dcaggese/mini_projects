5a667adbd7d066d58f2822c2cee91383a0ead110
Dominic Caggese <dcaggese@live.com>
2023-07-14 22:55:18 -0400

    science questions and question values added

--- quiz_game.py
+++ quiz_game.py
@@ -35,32 +35,32 @@ HISTORY_QUESTIONS = {
 
 SCIENCE_QUESTIONS = {
      'very easy': {
-          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          1: "What is the chemical symbol for the element oxygen?\n1. O\n2. Oc\n3. Oh\n4. Ox",
+          2: "Which planet is known as the 'Red Planet'?\n1. Venus\n2. Mars\n3. Jupiter\n4. Saturn",
+          3: "What is the process by which plants convert sunlight into energy called?\n1. Photosynthesis\n2. Respiration\n3. Fermentation\n4. Combustion",
+          4: "What is the SI unit for measuring electric current?\n1. Ampere\n2. Volt\n3. Ohm\n4. Watt",
+          5: "What is the smallest unit of matter?\n1. Atom\n2. Molecule\n3. Electron\n4. Proton",
      },
      'easy': {
-          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          1: "What is the process of a solid changing directly into a gas called?\n1. Condensation\n2. Sublimation\n3. Evaporation\n4. Melting",
+          2: "Which scientist is known for developing the theory of general relativity?\n1. Isaac Newton\n2. Albert Einstein\n3. Stephan Hawking\n4. Nikola Tesla",
+          3: "Which component of blood is responsible for carrying oxygen?\n1. Red blood cells\n2. White blood cells\n3. Platelets\n4. Plasma",
+          4: "What is the atomic number of carbon?\n1. 4\n2. 8\n3. 6\n4. 12",
+          5: "Which gas makes up the majority of Earth's atmosphere?\n1. Nitrogen\n2. Oxygen\n3. Carbon Dioxide\n4. Hydrogen",
      },
      'medium': {
-          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          1: "What is the study of earthquakes called?\n1. Geology\n2. Meteorology\n3. Seismology\n4. Volcanology",
+          2: "Which type of electromagnetic radiation has the shortest wavelength?\n1. Radio waves\n2. X-rays\n3. Infrared\n4. Gamma rays",
+          3: "What is the process by which plants release water vapor through their leaves?\n1. Transpiration\n2. Osmosis\n3. Respiration\n4. Diffusion",
+          4: "Which subatomic particle carries a negative charge?\n1. Proton\n2. Electron\n3. Neutron\n4. Photon",
+          5: "What is the study of fossils called?\n1. Paleontology\n2. Anthropology\n3. Archaeology\n4. Geology",
      },
      'hard': {
-          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
-          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          1: "What is the process of converying a solid directly into a liquid called?\n1. Condensation\n2. Evaporation\n3. Melting\n4. Freezing",
+          2: "Which scientist proposed the theory of evolution by natural selection?\n1. Gregor Mendel\n2. Charles Darwin\n3. Louis Pasteur\n4. Marie Curie",
+          3: "What is the largest planet in our solar system?\n1. Jupiter\n2. Saturn\n3. Uranus\n4. Neptune",
+          4: "Which particle in an atom has a positive charge?\n1. Neutron\n2. Proton\n3. Electron\n4. Nucleus",
+          5: "Which force holds the nucleus of an atom together?\n1. Gravitational force\n2. Electromagnetic force\n3. Strong nuclear force\n4. Weak nuclear force",
      }
 }
 
@@ -97,33 +97,40 @@ HISTORY_ANSWERS = {
 
 SCIENCE_ANSWERS = {
      'very easy': {
-          1: "",
-          2: "",
-          3: "",
-          4: "",
-          5: "",
+          1: 1,
+          2: 2,
+          3: 1,
+          4: 1,
+          5: 1,
      },
      'easy': {
-          1: "",
-          2: "",
-          3: "",
-          4: "",
-          5: "",
+          1: 2,
+          2: 2,
+          3: 1,
+          4: 3,
+          5: 1,
      },
      'medium': {
-          1: "",
-          2: "",
-          3: "",
-          4: "",
-          5: "",
+          1: 3,
+          2: 4,
+          3: 1,
+          4: 2,
+          5: 1,
      },
      'hard': {
-          1: "",
-          2: "",
-          3: "",
-          4: "",
-          5: "",
+          1: 3,
+          2: 2,
+          3: 1,
+          4: 2,
+          5: 3,
      }
+}
+
+QUESTION_VALUE = {
+     'very easy': 1,
+     'easy': 2,
+     'medium': 3,
+     'hard': 4
 }
 
 def main():
