4837f64e9590cd8e73da05928841992bceaf8aa9
Dominic Caggese <dcaggese@live.com>
2023-07-14 22:24:08 -0400

    added all history questions

--- quiz_game.py
+++ quiz_game.py
@@ -2,18 +2,142 @@ CATEGORIES = {
     1: 'history',
     2: 'science'
 }
+HISTORY_QUESTIONS = {
+     'very easy': {
+          1: "Who is credited with discovering America?\n1. Napoleon\n2. Christopher Columbus\n3.Mozart\n4. Lewis and Clark",
+          2: "In which year did World War II end?\n1. 1935\n2. 1942\n 3. 1943\n4. 1945",
+          3: "Who was the first President of the United States?\n1.Thomas Jefferson\n2.Ben Franklin\n3.George Washington\n4. Andrew Hamilton",
+          4: "Who was the leader of the Civil Rights Movement in the United States?\n1.Martin Luther King Jr.\n2. Malcolm X\n3.Bernie Sanders\n4. Dr. Cornell West",
+          5: "Which country built the Great Wall?\n1. Mongolia\n2. China\n3. Japan\n4. Korea"
+     },
+     'easy': {
+          1: "Who painted the Mona Lisa?\n1. Leonardo da Vinci\n2. Pablo Picasso\n3. Michelangelo\n4. Vincent Van Gogh",
+          2: "Who was the President of the United States during the Cuban Missle Crisis?\n1. George Bush\n2. Richard Nixon\n3. John F. Kennedy\n4. Bill Clinton",
+          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          4: "Who was President during the Civil War?\n1. Andrew Johnson\n2. Abraham Lincoln\n3. Ulysses S. Grant\n4. Bill Clinton",
+          5: "What city is known as the birthplace of the American Revolution?\n1. Boston\n2. New York\n3. Philadelphia\n4. Washington D.C.",
+     },
+     'medium': {
+          1: "What event led to the United States joining WWII?\n1. Twin Towers\n2. Cuban Missile Crisis\n3. Pearl Harbor\n4. Boston Massacre",
+          2: "Who first walked on the moon?\n1. Neil Armstrong\n2. Buzz Aldrin\n3. David R. Scott\n4. Alan B. Shepard",
+          3: "Which state was the first to be admitted to the Union?\n1. New York\n2. Virginia\n3. Delaware\n4. Massachusetts",
+          4: "Who is considered the founding father of the American Constitution?\n1. Alexander Hamilton\n2. James Madison\n3. Thomas Jefferson\n4. George Washington",
+          5: "Which President authorized the dropping of the atomic bombs on the Hiroshima and Nagasaki?\n1. Franklin D Roosevelt\n2. Harry S. Truman\n3. Dwight D Eisenhower\n4. John F. Kennedy",
+     },
+     'hard': {
+          1: "Which president was in office during the Great Depression and WWII?\n1. Herbert Hoover\n2. Franklin D. Roosevelt\n3. Woodrow Wilson\n4. Harry S. Truman",
+          2: "Which US president implemented the polict of 'Vietnamization' during the Vietnam War?\n1. Richard Nixon\n2. Lyndon B. Johnson\n3. John F. Kennedy\n4. Dwight D. Eisenhower",
+          3: "Which city was the site of the first shots fired in the American Civil War?\n1. Charleston, South Carolina\n2. Richmond, Virginia\n3. Atlanta, Georgia\n4. Fort Sumter, South Carolina",
+          4: "Which landmark Supreme Court xase legalized same sex-marriage nationwide in the United States?\n1. Obergefell v. Hodges\n2. Roe v. Wade\n3. Citizens United v. FEC\n4. Brown v. Board of Education",
+          5: "Which Supreme Court case established the principle of 'seperate but equal'?\n1. Brown v. Board of Education\n2. Dred Scott v. Sandford\n3. Roe v. Wade\n4. Plessy v. Ferguson",
+     }
+}
+
+SCIENCE_QUESTIONS = {
+     'very easy': {
+          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+     },
+     'easy': {
+          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+     },
+     'medium': {
+          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+     },
+     'hard': {
+          1: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          2: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          4: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+          5: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
+     }
+}
+
+HISTORY_ANSWERS = {
+     'very easy': {
+          1: 2,
+          2: 4,
+          3: 3,
+          4: 1,
+          5: 2
+     },
+     'easy': {
+          1: 1,
+          2: 3,
+          3: 4,
+          4: 2,
+          5: 1,
+     },
+     'medium': {
+          1: 3,
+          2: 1,
+          3: 3,
+          4: 3,
+          5: 2,
+     },
+     'hard': {
+          1: 2,
+          2: 1,
+          3: 4,
+          4: 1,
+          5: 4,
+     }
+}
+
+SCIENCE_ANSWERS = {
+     'very easy': {
+          1: "",
+          2: "",
+          3: "",
+          4: "",
+          5: "",
+     },
+     'easy': {
+          1: "",
+          2: "",
+          3: "",
+          4: "",
+          5: "",
+     },
+     'medium': {
+          1: "",
+          2: "",
+          3: "",
+          4: "",
+          5: "",
+     },
+     'hard': {
+          1: "",
+          2: "",
+          3: "",
+          4: "",
+          5: "",
+     }
+}
 
 def main():
         welcome()
         selection = category_selection(CATEGORIES)
         print(CATEGORIES[selection])
 
-
+#welcome the user and explain how to play
 def welcome():
         print("Welcome to Quiz Game! Use the number keys 1-4 to select your answer.")
         print("The game will consist of 4 random questions from the selected category.")
         print("Good luck!")
 
+#let the user select a category of questions
 def category_selection(categories):
     for category in categories:
         print(f"Press {category} to answer {categories[category]} based questions.")
@@ -25,5 +149,11 @@ def category_selection(categories):
             return int(selection)
         
         print("Enter a valid number to select a category.")
+
+#randomally select questions from the selected category
+def give_questions(questions, answers):
+     
+
+     return
 
 main()
