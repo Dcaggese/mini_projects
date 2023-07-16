000632e2520f8d99c028d81c37bb3d0f7fb4d6e9
Dominic Caggese <dcaggese@live.com>
2023-07-15 21:00:43 -0400

    quiz game complete

--- quiz_game.py
+++ quiz_game.py
@@ -1,30 +1,33 @@
+import random
+
 CATEGORIES = {
     1: 'history',
     2: 'science'
 }
+
 HISTORY_QUESTIONS = {
-     'very easy': {
+     1: {
           1: "Who is credited with discovering America?\n1. Napoleon\n2. Christopher Columbus\n3.Mozart\n4. Lewis and Clark",
           2: "In which year did World War II end?\n1. 1935\n2. 1942\n 3. 1943\n4. 1945",
           3: "Who was the first President of the United States?\n1.Thomas Jefferson\n2.Ben Franklin\n3.George Washington\n4. Andrew Hamilton",
           4: "Who was the leader of the Civil Rights Movement in the United States?\n1.Martin Luther King Jr.\n2. Malcolm X\n3.Bernie Sanders\n4. Dr. Cornell West",
           5: "Which country built the Great Wall?\n1. Mongolia\n2. China\n3. Japan\n4. Korea"
      },
-     'easy': {
+     2: {
           1: "Who painted the Mona Lisa?\n1. Leonardo da Vinci\n2. Pablo Picasso\n3. Michelangelo\n4. Vincent Van Gogh",
           2: "Who was the President of the United States during the Cuban Missle Crisis?\n1. George Bush\n2. Richard Nixon\n3. John F. Kennedy\n4. Bill Clinton",
           3: "When did the Civil War start?\n1. 1832\n2. 1855\n3. 1863\n4. 1861",
           4: "Who was President during the Civil War?\n1. Andrew Johnson\n2. Abraham Lincoln\n3. Ulysses S. Grant\n4. Bill Clinton",
           5: "What city is known as the birthplace of the American Revolution?\n1. Boston\n2. New York\n3. Philadelphia\n4. Washington D.C.",
      },
-     'medium': {
+     3: {
           1: "What event led to the United States joining WWII?\n1. Twin Towers\n2. Cuban Missile Crisis\n3. Pearl Harbor\n4. Boston Massacre",
           2: "Who first walked on the moon?\n1. Neil Armstrong\n2. Buzz Aldrin\n3. David R. Scott\n4. Alan B. Shepard",
           3: "Which state was the first to be admitted to the Union?\n1. New York\n2. Virginia\n3. Delaware\n4. Massachusetts",
           4: "Who is considered the founding father of the American Constitution?\n1. Alexander Hamilton\n2. James Madison\n3. Thomas Jefferson\n4. George Washington",
           5: "Which President authorized the dropping of the atomic bombs on the Hiroshima and Nagasaki?\n1. Franklin D Roosevelt\n2. Harry S. Truman\n3. Dwight D Eisenhower\n4. John F. Kennedy",
      },
-     'hard': {
+     4: {
           1: "Which president was in office during the Great Depression and WWII?\n1. Herbert Hoover\n2. Franklin D. Roosevelt\n3. Woodrow Wilson\n4. Harry S. Truman",
           2: "Which US president implemented the polict of 'Vietnamization' during the Vietnam War?\n1. Richard Nixon\n2. Lyndon B. Johnson\n3. John F. Kennedy\n4. Dwight D. Eisenhower",
           3: "Which city was the site of the first shots fired in the American Civil War?\n1. Charleston, South Carolina\n2. Richmond, Virginia\n3. Atlanta, Georgia\n4. Fort Sumter, South Carolina",
@@ -34,28 +37,28 @@ HISTORY_QUESTIONS = {
 }
 
 SCIENCE_QUESTIONS = {
-     'very easy': {
+     1: {
           1: "What is the chemical symbol for the element oxygen?\n1. O\n2. Oc\n3. Oh\n4. Ox",
           2: "Which planet is known as the 'Red Planet'?\n1. Venus\n2. Mars\n3. Jupiter\n4. Saturn",
           3: "What is the process by which plants convert sunlight into energy called?\n1. Photosynthesis\n2. Respiration\n3. Fermentation\n4. Combustion",
           4: "What is the SI unit for measuring electric current?\n1. Ampere\n2. Volt\n3. Ohm\n4. Watt",
           5: "What is the smallest unit of matter?\n1. Atom\n2. Molecule\n3. Electron\n4. Proton",
      },
-     'easy': {
+     2: {
           1: "What is the process of a solid changing directly into a gas called?\n1. Condensation\n2. Sublimation\n3. Evaporation\n4. Melting",
           2: "Which scientist is known for developing the theory of general relativity?\n1. Isaac Newton\n2. Albert Einstein\n3. Stephan Hawking\n4. Nikola Tesla",
           3: "Which component of blood is responsible for carrying oxygen?\n1. Red blood cells\n2. White blood cells\n3. Platelets\n4. Plasma",
           4: "What is the atomic number of carbon?\n1. 4\n2. 8\n3. 6\n4. 12",
           5: "Which gas makes up the majority of Earth's atmosphere?\n1. Nitrogen\n2. Oxygen\n3. Carbon Dioxide\n4. Hydrogen",
      },
-     'medium': {
+     3: {
           1: "What is the study of earthquakes called?\n1. Geology\n2. Meteorology\n3. Seismology\n4. Volcanology",
           2: "Which type of electromagnetic radiation has the shortest wavelength?\n1. Radio waves\n2. X-rays\n3. Infrared\n4. Gamma rays",
           3: "What is the process by which plants release water vapor through their leaves?\n1. Transpiration\n2. Osmosis\n3. Respiration\n4. Diffusion",
           4: "Which subatomic particle carries a negative charge?\n1. Proton\n2. Electron\n3. Neutron\n4. Photon",
           5: "What is the study of fossils called?\n1. Paleontology\n2. Anthropology\n3. Archaeology\n4. Geology",
      },
-     'hard': {
+     4: {
           1: "What is the process of converying a solid directly into a liquid called?\n1. Condensation\n2. Evaporation\n3. Melting\n4. Freezing",
           2: "Which scientist proposed the theory of evolution by natural selection?\n1. Gregor Mendel\n2. Charles Darwin\n3. Louis Pasteur\n4. Marie Curie",
           3: "What is the largest planet in our solar system?\n1. Jupiter\n2. Saturn\n3. Uranus\n4. Neptune",
@@ -65,28 +68,28 @@ SCIENCE_QUESTIONS = {
 }
 
 HISTORY_ANSWERS = {
-     'very easy': {
+     1: {
           1: 2,
           2: 4,
           3: 3,
           4: 1,
           5: 2
      },
-     'easy': {
+     2: {
           1: 1,
           2: 3,
           3: 4,
           4: 2,
           5: 1,
      },
-     'medium': {
+     3: {
           1: 3,
           2: 1,
           3: 3,
           4: 3,
           5: 2,
      },
-     'hard': {
+     4: {
           1: 2,
           2: 1,
           3: 4,
@@ -96,28 +99,28 @@ HISTORY_ANSWERS = {
 }
 
 SCIENCE_ANSWERS = {
-     'very easy': {
+     1: {
           1: 1,
           2: 2,
           3: 1,
           4: 1,
           5: 1,
      },
-     'easy': {
+     2: {
           1: 2,
           2: 2,
           3: 1,
           4: 3,
           5: 1,
      },
-     'medium': {
+     3: {
           1: 3,
           2: 4,
           3: 1,
           4: 2,
           5: 1,
      },
-     'hard': {
+     4: {
           1: 3,
           2: 2,
           3: 1,
@@ -127,16 +130,24 @@ SCIENCE_ANSWERS = {
 }
 
 QUESTION_VALUE = {
-     'very easy': 1,
-     'easy': 2,
-     'medium': 3,
-     'hard': 4
+     1: 1,
+     2: 2,
+     3: 3,
+     4: 4
 }
 
 def main():
-        welcome()
-        selection = category_selection(CATEGORIES)
-        print(CATEGORIES[selection])
+        while True:
+            welcome()
+            selection = category_selection(CATEGORIES)
+            if selection == 1:
+                score = give_questions(HISTORY_QUESTIONS, HISTORY_ANSWERS, QUESTION_VALUE)
+            elif selection == 2:
+                score =give_questions(SCIENCE_QUESTIONS, SCIENCE_ANSWERS, QUESTION_VALUE)
+            print(f"You scored {score} points! Press 'enter' to play again or 'q' to quit.")
+            play = input("Press enter to play again or 'q' to quit.")
+            if play == 'q':
+                 break
 
 #welcome the user and explain how to play
 def welcome():
@@ -158,9 +169,21 @@ def category_selection(categories):
         print("Enter a valid number to select a category.")
 
 #randomally select questions from the selected category
-def give_questions(questions, answers):
-     
+def give_questions(questions, answers, value):
+    final_score = 0
+    for key in questions:
+        question = random.randint(1,5)
+        print(questions[key][question])
+        while True:
+          answer = int(input("Enter your answer. (1-4)"))
+          if answer >= 1 and answer <= 4:
+               break
+        if answer == answers[key][question]:
+            final_score += value[key]
+            print(f"Correct! Your score is: {final_score}")
+        else:
+             print("Wrong answer!")
 
-     return
+    return final_score
 
 main()
